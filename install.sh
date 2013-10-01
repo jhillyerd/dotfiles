abcbf65a69a7745bd9c82ea066242b524a35a0fb
James Hillyerd <james@hillyerd.com>
2013-10-01 14:57:56 -0700

    Add ctags

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing to $HOME"
 
-for file in cvsignore gitconfig tmux.conf zshrc; do
+for file in ctags cvsignore gitconfig tmux.conf zshrc; do
     echo Linking $file
     ln -s ".dotfiles/$file" "$HOME/.$file"
 done
