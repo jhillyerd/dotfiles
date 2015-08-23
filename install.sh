ccdefa59ede75ada48f2166c599d686ba4acca3d
James Hillyerd <james@hillyerd.com>
2015-08-23 00:04:48 -0700

    install.sh changes
    - functionize
    - link config.fish

--- install.sh
+++ install.sh
@@ -3,22 +3,38 @@
 [[ "$TRACE" ]] && set -x
 set -eo pipefail
 
-echo "Installing to $HOME"
+main() {
+  echo "Installing to $HOME"
 
-# Backup /etc/skel provided files
-for file in bash_profile bashrc; do
-  target="$HOME/.$file"
-  if [[ -f "$target" && ! -h "$target" ]]; then
-    mv "$target" "$target.bak"
+  # Backup /etc/skel provided files
+  for file in bash_profile bashrc; do
+    local target="$HOME/.$file"
+    if [[ -f "$target" && ! -h "$target" ]]; then
+      mv "$target" "$target.bak"
+    fi
+  done
+
+  for file in bash_profile bashrc ctags cvsignore gitconfig tmux.conf zshrc zsh-custom; do
+    linkfile "$file" "$HOME/.$file"
+  done
+
+  linkfile "config.fish" "$HOME/.config/fish/config.fish"
+}
+
+linkfile() {
+  declare file="$1" target="$2"
+  local dirname="${target%/*}"
+
+  if [[ ! -d "$dirname" ]]; then
+    mkdir -p "$dirname"
   fi
-done
 
-for file in bash_profile bashrc ctags cvsignore gitconfig tmux.conf zshrc zsh-custom; do
-  target="$HOME/.$file"
   if [ -e "$target" ]; then
     echo "$target exists; skipping"
   else
     echo "Linking $file to $target"
-    ln -s ".dotfiles/$file" "$target"
+    ln -s "$HOME/.dotfiles/$file" "$target"
   fi
-done
+}
+
+main
