106e59b42e2c45cd1f81f4ef1fd157c7e88cfd13
James Hillyerd <james@hillyerd.com>
2016-12-31 00:37:24 +0000

    Add -s flag for ssh on ghclone

--- omf/functions/ghclone.fish
+++ omf/functions/ghclone.fish
@@ -1,3 +1,14 @@
-function ghclone -a repo args -d "Clone a github repo"
-  git clone "https://jhillyerd@github.com/$repo" $args
+function ghclone -a repo directory -d "Clone a github repo"
+  # -s argument uses ssh instead of https
+  if test $repo = "-s"
+    set repo $argv[2]
+    if test (count $argv) -ge 3
+      set directory $argv[3]
+    else
+      set -e directory
+    end
+    git clone "git@github.com:"$repo".git" $directory
+  else
+    git clone "https://jhillyerd@github.com/$repo" $directory
+  end
 end
