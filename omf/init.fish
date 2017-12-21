72884fa49099db97e2036f1980b4536db76c8de9
James Hillyerd <james@hillyerd.com>
2017-12-21 09:27:18 -0800

    Maked EDITOR global, source Google fish cfg

--- omf/init.fish
+++ omf/init.fish
@@ -1,8 +1,4 @@
-# Environment setup
-if not set -q EDITOR
-  set -x EDITOR vim
-end
-
+# Path setup
 if not contains . $CDPATH
   set -x CDPATH . $CDPATH
 end
@@ -15,11 +11,20 @@ if test -d $HOME/bin
   set -x PATH $HOME/bin $PATH
 end
 
+# Environment setup
 if test $TERM = 'xterm'
   set -xg TERM 'xterm-256color'
 end
 
-#echo INIT $SHLVL
+if test -z $EDITOR
+  set -xg EDITOR vim
+end
+
 source $OMF_CONFIG/golang.fish
 
+if test -r /google/data/ro/teams/fish/google.fish
+  source /google/data/ro/teams/fish/google.fish
+end
+
+# Abbreviations
 abbr -a dk docker
