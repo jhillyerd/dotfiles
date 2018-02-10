93795caad622b75ae422c8a1798ca021a566e3fb
James Hillyerd <james@hillyerd.com>
2018-02-10 14:58:09 -0800

    Use shared window group for tmux

--- omf/functions/t.fish
+++ omf/functions/t.fish
@@ -1,7 +1,9 @@
 function t -d "Start or attach to tmux session"
+  # Creates a new session, but connects to same window group main
+  set -l args new-session -t main
   if test -x /usr/bin/tmx
-    tmx a; or tmx
+    tmx $args
   else
-    tmux a; or tmux
+    tmux $args
   end
 end
