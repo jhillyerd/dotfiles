4ce6099587f57d23fbdfaf89ab484d3db00bca90
James Hillyerd <james@hillyerd.com>
2018-02-12 18:01:52 -0800

    Switch to tmx2

--- omf/functions/t.fish
+++ omf/functions/t.fish
@@ -1,8 +1,8 @@
 function t -d "Start or attach to tmux session"
   # Creates a new session, but connects to same window group main
   set -l args new-session -t main
-  if test -x /usr/bin/tmx
-    tmx $args
+  if test -x /usr/bin/tmx2
+    tmx2 $args
   else
     tmux $args
   end
