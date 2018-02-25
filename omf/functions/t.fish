c5dc715c7d7f75940fb783577f4d5ca16f56471e
James Hillyerd <james@hillyerd.com>
2018-02-25 08:31:33 -0800

    t now prompts for ssh key

--- omf/functions/t.fish
+++ omf/functions/t.fish
@@ -1,4 +1,9 @@
 function t -d "Start or attach to tmux session"
+  # Prompt for ssh-key if needed
+  if command -q -s ssh-add; and not ssh-add -l >/dev/null
+    ssh-add
+  end
+
   # Creates a new session, but connects to same window group main
   set -l args new-session -t main
   if test -x /usr/bin/tmx2
