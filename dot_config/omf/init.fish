7e9e90bb34bfc97ec058359aca854b6187aa9baa
James Hillyerd <james@hillyerd.com>
2018-11-24 13:12:52 -0800

    Update chezmoi paths, clean out some cruft

--- dot_config/omf/init.fish
+++ dot_config/omf/init.fish
@@ -1,4 +1,4 @@
-function _bin_path -a path -d "Add PATH if not already present"
+function _bin_path -a path -d "Prepend path if not already present"
   if not string match -q $path $PATH; and test -d $path
     set -x PATH $path $PATH
   end
@@ -12,7 +12,7 @@ end
 _bin_path /usr/local/bin
 _bin_path /usr/local/node/bin
 _bin_path $HOME/google-cloud-sdk/bin
-_bin_path $HOME/.dotfiles/bin
+_bin_path $HOME/.scripts
 _bin_path $HOME/bin
 
 # Environment setup
@@ -30,7 +30,7 @@ abbr --add dc docker-compose
 abbr --add dk docker
 abbr --add dkclean docker container prune --force
 abbr --add dke docker exec -it
-abbr --add dki docker run -itP
+abbr --add dki docker run -itP --rm
 
 # Cleanup
 functions -e _bin_path
