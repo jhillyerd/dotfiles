2eee49b5b8be8ce503654c37ab4c44dcf92c5574
James Hillyerd <james@hillyerd.com>
2019-02-18 08:33:28 -0800

    t: allow for specifying session name

--- dot_config/omf/functions/t.fish
+++ dot_config/omf/functions/t.fish
@@ -1,11 +1,16 @@
-function t -d "Start or attach to tmux session"
+function t -a session -d "Start or attach to tmux session"
+  if test "$session" = ""
+    set session main
+  end
+
   # Prompt for ssh-key if needed
   if command -q -s ssh-add; and not ssh-add -l >/dev/null
     ssh-add
   end
 
   # Creates a new session, but connects to same window group main
-  set -l args new-session -t main
+  set -l args new-session -t $session
+
   if test -x /usr/bin/tmx2
     tmx2 $args
   else
