24ed16e2afaf4a4f4b17d0feb5f557fbe64c791c
James Hillyerd <james@hillyerd.com>
2020-04-24 08:45:57 -0700

    Use global abbreviations

--- dot_config/omf/init.fish
+++ dot_config/omf/init.fish
@@ -27,16 +27,16 @@ set -xg VISUAL vim
 source $OMF_CONFIG/golang.fish
 
 # Abbreviations
-abbr --add ccd chezmoi cd
-abbr --add cea chezmoi edit --apply --diff
-abbr --add cu chezmoi update -v
-abbr --add dc docker-compose
-abbr --add dk docker
-abbr --add dkclean docker container prune --force
-abbr --add dke docker exec -it
-abbr --add dki docker run -itP --rm
-abbr --add nfish "nix-shell --command 'SHELL=fish; exec fish'"
-abbr --add nfish! "nix-shell -I nixpkgs=/home/james/nixpkgs --command 'SHELL=fish; exec fish'"
+abbr -a -g ccd chezmoi cd
+abbr -a -g cea chezmoi edit --apply --diff
+abbr -a -g cu chezmoi update -v
+abbr -a -g dc docker-compose
+abbr -a -g dk docker
+abbr -a -g dkclean docker container prune --force
+abbr -a -g dke docker exec -it
+abbr -a -g dki docker run -itP --rm
+abbr -a -g nfish "nix-shell --command 'SHELL=fish; exec fish'"
+abbr -a -g nfish! "nix-shell -I nixpkgs=/home/james/nixpkgs --command 'SHELL=fish; exec fish'"
 
 # Key bindings
 bind \cp fzf-file-widget
