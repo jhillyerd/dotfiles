f4198dd33a427d696804468e045ecad54ba8081f
James Hillyerd <james@hillyerd.com>
2020-08-29 12:40:36 -0700

    add installer for basic nix-env

---
+++ dot_scripts/executable_install-nixpkgs-wsl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+nix-env -iA nixpkgs.chezmoi
+nix-env -iA nixpkgs.ctags
+nix-env -iA nixpkgs.neovim
+nix-env -iA nixpkgs.nodejs-12_x
+nix-env -iA nixpkgs.ripgrep
+nix-env -iA nixpkgs.tree
