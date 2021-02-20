c6ded08cd7deefe25ef278ec855671f338d32bf8
James Hillyerd <james@hillyerd.com>
2021-02-20 09:27:07 -0800

    unpin nodejs version

--- dot_scripts/executable_install-nixpkgs-wsl.sh
+++ dot_scripts/executable_install-nixpkgs-wsl.sh
@@ -4,7 +4,7 @@ nix-env -iA nixpkgs.chezmoi
 nix-env -iA nixpkgs.ctags
 nix-env -iA nixpkgs.gitAndTools.gh
 nix-env -iA nixpkgs.neovim
-nix-env -iA nixpkgs.nodejs-12_x
+nix-env -iA nixpkgs.nodejs
 nix-env -iA nixpkgs.starship
 nix-env -iA nixpkgs.ripgrep
 nix-env -iA nixpkgs.tmux
