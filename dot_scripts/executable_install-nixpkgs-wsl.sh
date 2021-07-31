8c7e0422ddabd0139a4814701289dab205f5b80c
James Hillyerd <james@hillyerd.com>
2021-07-31 11:02:48 -0700

    add bat and jq to wsl

--- dot_scripts/executable_install-nixpkgs-wsl.sh
+++ dot_scripts/executable_install-nixpkgs-wsl.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+nix-env -iA nixpkgs.bat
 nix-env -iA nixpkgs.chezmoi
 nix-env -iA nixpkgs.ctags
 nix-env -iA nixpkgs.gitAndTools.gh
+nix-env -iA nixpkgs.jq
 nix-env -iA nixpkgs.neovim
 nix-env -iA nixpkgs.nodejs
 nix-env -iA nixpkgs.starship
