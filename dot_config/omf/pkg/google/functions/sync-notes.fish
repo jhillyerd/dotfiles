b030bdf3632d9143b656a32f6e4b8beded29ba50
James Hillyerd <james@hillyerd.com>
2022-04-29 18:04:41 -0700

    improve sync-notes

--- dot_config/omf/pkg/google/functions/sync-notes.fish
+++ dot_config/omf/pkg/google/functions/sync-notes.fish
@@ -3,8 +3,13 @@ function sync-notes
   set -l dirs notes work-notes
 
   for dir in $dirs
+    if ! test -x $dir/quicksync.sh
+      echo "### Skipping: $dir"
+      continue
+    end
+
     pushd $dir
-    echo "###" (pwd)
+    echo "### Syncing: $dir"
     ./quicksync.sh
     popd
   end
