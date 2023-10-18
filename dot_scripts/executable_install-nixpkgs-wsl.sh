489e668fbb5a8f1c858bbdb95d8e06921f44f075
James Hillyerd <james@hillyerd.com>
2023-10-18 15:34:10 -0700

    use nix profile for WSL nixpkgs

--- dot_scripts/executable_install-nixpkgs-wsl.sh
+++ dot_scripts/executable_install-nixpkgs-wsl.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-nix-env -iA \
-  nixpkgs.bat \
-  nixpkgs.chezmoi \
-  nixpkgs.ctags \
-  nixpkgs.gitAndTools.gh \
-  nixpkgs.gitAndTools.git-absorb \
-  nixpkgs.jq \
-  nixpkgs.lazygit \
-  nixpkgs.neovim \
-  nixpkgs.nixfmt \
-  nixpkgs.nmap \
-  nixpkgs.nodejs \
-  nixpkgs.starship \
-  nixpkgs.ripgrep \
-  nixpkgs.tmux \
-  nixpkgs.tree \
-  nixpkgs.whois
+nix profile install \
+  nixpkgs#bat \
+  nixpkgs#chezmoi \
+  nixpkgs#ctags \
+  nixpkgs#gitAndTools.gh \
+  nixpkgs#gitAndTools.git-absorb \
+  nixpkgs#jq \
+  nixpkgs#lazygit \
+  nixpkgs#neovim \
+  nixpkgs#nixfmt \
+  nixpkgs#nmap \
+  nixpkgs#nodejs \
+  nixpkgs#starship \
+  nixpkgs#ripgrep \
+  nixpkgs#tmux \
+  nixpkgs#tree \
+  nixpkgs#whois
