b1ac776eed967e9fdb39569f83781dfcb205d23a
James Hillyerd <james@hillyerd.com>
2023-11-20 12:06:42 -0800

    cme: fzf updates cmdline instead of executing directly
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- dot_config/omf/functions/cme.fish
+++ dot_config/omf/functions/cme.fish
@@ -1,6 +1,9 @@
 function cme -a editpath -d "Chezmoi edit file; prompts with fzf"
   if test -z "$editpath"
     set editpath (chezmoi managed --include files --no-pager | fzf)
+    history delete --exact --case-sensitive cme
+    commandline --replace "cme $editpath"
+    return
   end
 
   pushd ~
