9ed8d2a0fe94f42ca02e65b55c2e0c207df69872
James Hillyerd <james@hillyerd.com>
2025-04-12 10:42:45 -0700

    customize sway
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- dot_scripts/executable_after-sway-start.sh
+++ dot_scripts/executable_after-sway-start.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-LOG="$HOME/.yambar.log"
 killall yambar
-yambar >$LOG 2>&1 & disown
+yambar >"$HOME/.yambar.log" 2>&1 & disown
+
+lock="$HOME/.scripts/lock-screen-sway.sh"
+killall swayidle
+swayidle -w \
+  lock $lock \
+  timeout 300 $lock \
+  timeout 600 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
+  before-sleep $lock & disown
+
+killall wl-paste
+wl-paste -t text --watch clipman store --no-persist & disown
