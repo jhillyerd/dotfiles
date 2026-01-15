cb80387007ca52efea0e4ef133964d3e90affa6a
James Hillyerd <james@hillyerd.com>
2026-01-14 19:38:35 -0800

    t: ssh-add not useful here
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- dot_config/omf/functions/t.fish
+++ dot_config/omf/functions/t.fish
@@ -7,11 +7,6 @@ function t -a session -d "Start or attach to tmux session"
     end
   end
 
-  # Prompt for ssh-key if needed
-  if command -q -s ssh-add; and not ssh-add -l >/dev/null
-    ssh-add
-  end
-
   # Creates a new session, but connects to same window group
   tmux new-session -t $session
 end
