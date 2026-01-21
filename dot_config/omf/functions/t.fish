04da91aa803e1e9a5a87c9739e066642acb8d61a
James Hillyerd <james@hillyerd.com>
2026-01-21 12:20:12 -0800

    ssh: auth_sock follows last connection
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- dot_config/omf/functions/t.fish
+++ dot_config/omf/functions/t.fish
@@ -7,6 +7,12 @@ function t -a session -d "Start or attach to tmux session"
     end
   end
 
+  # Allow SSH agent forwarding to follow last inbound connection.
+  set inbound_auth_sock ~/.ssh/ssh_auth_sock
+  if test -S $inbound_auth_sock
+    set -x SSH_AUTH_SOCK $inbound_auth_sock
+  end
+
   # Creates a new session, but connects to same window group
   tmux new-session -t $session
 end
