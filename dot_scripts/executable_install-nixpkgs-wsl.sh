8ea429525bc6ef650db48ec368bbec29e406f290
James Hillyerd <james@hillyerd.com>
2026-03-03 03:49:52 +0000

    script: update install-nixpkgs-wsl.sh
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- dot_scripts/executable_install-nixpkgs-wsl.sh
+++ dot_scripts/executable_install-nixpkgs-wsl.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-nix profile install \
+nix profile add \
   nixpkgs#bat \
   nixpkgs#chezmoi \
   nixpkgs#ctags \
   nixpkgs#direnv \
   nixpkgs#fzf \
-  nixpkgs#gitAndTools.gh \
-  nixpkgs#gitAndTools.git-absorb \
+  nixpkgs#gh \
+  nixpkgs#glow \
   nixpkgs#jq \
   nixpkgs#lazygit \
-  nixpkgs#lf \
-  nixpkgs#sumneko-lua-language-server \
+  nixpkgs#lua-language-server \
   nixpkgs#neovim \
   nixpkgs#nixfmt \
   nixpkgs#nmap \
   nixpkgs#nodejs \
-  nixpkgs#mosh \
   nixpkgs#starship \
   nixpkgs#ripgrep \
   nixpkgs#tmux \
   nixpkgs#tree \
-  nixpkgs#whois
+  nixpkgs#watchexec \
+  nixpkgs#whois \
+  nixpkgs#yazi
